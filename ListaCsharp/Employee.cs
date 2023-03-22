0cdd95cabdd363041f4cc5df68c3ee95f9716438
Matheus Henrique <Matheus Henrique@ABN0403>
2023-03-22 09:08:00 -0300

    Adicionar arquivos de projeto.

---
+++ ListaCsharp/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ListaCsharp {
+    internal class Employee {
+        public int _id;
+        private string _name;
+        private decimal _salary;
+
+        public Employee(int id, string name, decimal salary) {
+            _id = id;
+            _name = name;
+            _salary = salary;
+        }
+        public void increaseSalary(decimal valor) {
+            _salary += valor;
+        }
+        public override string ToString() {
+            return _id
+                + ", "
+                + _name
+                + ", "
+                + _salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ListaCsharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using ListaCsharp;
+using System.Globalization;
+using System;
+using System.Collections.Generic;
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                decimal salary = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x._id == searchId);
+            if (emp != null) {
+                Console.Write("Enter the percentage: ");
+                decimal percentage = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.increaseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee obj in list) {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
